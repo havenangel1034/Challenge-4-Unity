88242ec385a216987c183c9f008d10eea5b4083c
Gabby <gabbie10112005@gmail.com>
2025-02-25 09:06:39 -0600

    Initial commit

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    public GameObject playerGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal"); // Assign the playerGoal reference
+    }
+
+    private Vector3 GetLookDirection()
+    {
+        return (playerGoal.transform.position - transform.position).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerGoal != null)
+        {
+            Vector3 lookDirection = GetLookDirection();
+            enemyRb.AddForce(lookDirection * 100 * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If the enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
